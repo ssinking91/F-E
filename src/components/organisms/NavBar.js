84004500dce34213c6f544b8e7feb427076d6006
김형래 <khr157929@gmail.com>
2021-12-29 12:57:57 +0900

    Naver Login

--- src/components/organisms/NavBar.js
+++ src/components/organisms/NavBar.js
@@ -44,8 +44,7 @@ export default function NavBar() {
             <Heading1
               style={{
                 margin: "auto",
-                position: "relative",
-                cursor: "pointer",
+                cursor: "default",
               }}
             >
               <HandleOn>
@@ -85,6 +84,7 @@ const HeaderWrap = styled.div`
 
 const HandleOn = styled.div`
   transition: opacity 1s;
+  position: relative;
 
   &:hover .handle {
     transition: 1s;
@@ -97,4 +97,8 @@ const HandleOff = styled.div`
   transition: 0.5s;
   opacity: 0;
   pointer-events: none;
+
+  position: absolute;
+  top: -20px;
+  left: -128px;
 `;
