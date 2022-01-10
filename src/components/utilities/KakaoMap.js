4ba7e69569151c681cac77f812323e9184d3b148
김형래 <khr157929@gmail.com>
2022-01-10 11:17:23 +0900

    임시저장

--- src/components/utilities/KakaoMap.js
+++ src/components/utilities/KakaoMap.js
@@ -53,14 +53,21 @@ export default function KakaoMap() {
 
     // 주소로 좌표를 검색합니다
     if (location) {
+      const clusterer = new kakao.maps.MarkerClusterer({
+        map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        minLevel: 4, // 클러스터 할 최소 지도 레벨
+        calculator: [3, 5, 10],
+        minClusterSize: 1,
+      });
+      kakao.maps.event.addListener(
+        clusterer,
+        "clusterover",
+        function (cluster) {
+          console.log(cluster);
+        }
+      );
       for (let i = 0; i < location.length; i++) {
-        const clusterer = new kakao.maps.MarkerClusterer({
-          map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
-          averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
-          minLevel: 4, // 클러스터 할 최소 지도 레벨
-          calculator: [3, 5, 10],
-          minClusterSize: 1,
-        });
         let marker;
         await geocoder.addressSearch(
           location[i],
@@ -112,13 +119,6 @@ export default function KakaoMap() {
                 }
               }
 
-              kakao.maps.event.addListener(
-                clusterer,
-                "clusterover",
-                function (cluster) {
-                  console.log(cluster);
-                }
-              );
               // kakao.maps.event.addListener(
               //   clusterer,
               //   "clusterclick",
