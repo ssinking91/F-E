eda534398f94beda5c597cdf4c116167fc0c01a9
kongom2 <kongom2@jsceno.com>
2022-01-11 23:21:04 +0900

    navbarsub

---
+++ src/components/molecules/TypeFilter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function TypeFilter() {
+  return <Div></Div>;
+}
+const Div = styled.div`
+  width: 100%;
+  height: 65px;
+`;
--- src/components/organisms/NavBarSub.js
+++ src/components/organisms/NavBarSub.js
@@ -1,16 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import TypeButton from "../molecules/TypeButton";
+import TypeFilter from "../molecules/TypeFilter";
 
 export default function NavBarSub() {
-  return <Div></Div>;
+  return (
+    <Div>
+      <TypeFilter></TypeFilter>
+      <TypeButton></TypeButton>
+    </Div>
+  );
 }
 
 const Div = styled.div`
   width: 100%;
   height: 65px;
   position: absolute;
+  display: flex;
   top: 97px;
   z-index: 999;
-  background-color: gray;
-  opacity: 0.1;
+  background-color: #fff;
+  border-bottom: 1px solid rgb(205, 205, 205);
+  //   opacity: 0.1;
 `;
