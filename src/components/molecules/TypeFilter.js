b7b0e67538cf368e43dc0202bbbe64996855a264
kongom2 <kongom2@jsceno.com>
2022-01-12 03:37:13 +0900

    분양가격 수정

--- src/components/molecules/TypeFilter.js
+++ src/components/molecules/TypeFilter.js
@@ -1,10 +1,42 @@
 import React from "react";
 import styled from "styled-components";
+import { Grid, Button, Text } from "../atoms/index";
 
 export default function TypeFilter() {
-  return <Div></Div>;
+  return (
+    <Div>
+      <Wrap>
+        <Icon></Icon>
+        <SearchInput type="search" placeholder="검색"></SearchInput>
+      </Wrap>
+    </Div>
+  );
 }
+const Icon = styled.div`
+  width: 30px;
+  height: 30px;
+  background-size: cover;
+  background-image: url("../../img/searchIcon.png");
+`;
 const Div = styled.div`
   width: 100%;
   height: 65px;
+  display: flex;
+  align-items: center;
+`;
+const Wrap = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+const SearchInput = styled.input`
+  width: 300px;
+  height: 36px;
+  padding: 0 10px;
+  border: 0px;
+  font-size: 14px;
+  &:focus {
+    outline: none;
+  }
 `;
