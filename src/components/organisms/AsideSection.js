f75c8452856decbbe0364cafb797a7bb6f38c225
김형래 <khr157929@gmail.com>
2022-01-13 13:47:55 +0900

    asidesection warning

--- src/components/organisms/AsideSection.js
+++ src/components/organisms/AsideSection.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Main2Card from "../organisms/Main2Card";
 
 export default function AsideSection() {
